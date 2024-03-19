1c1b1868ab744efcfe1770cd8b07b2fafdf1b208
dibmens <dibmens@gmail.com>
2024-03-19 22:39:58 +0200

    Create modal windows and add review functionality

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let bookTemplate = document.querySelector('.book');
 let form = document.forms[0];
 let myLibrary = [];
+let lastReview;
 
 // Book object constructor
-
 function Book(title, author, year, genre, pages, score) {
     this.title = title;
     this.author = author;
@@ -13,105 +13,91 @@ function Book(title, author, year, genre, pages, score) {
     this.score = score;
 }
 
-// Function for adding new book in myLibrary
-
-function addBookToLibrary() {
-
-    let bookVal = [];
-
-    for(i = 0; i < 6; i++){
-        bookVal.push(form.elements[i].value);
-    };
-
-    let book = new Book(bookVal[0],bookVal[1],bookVal[2],bookVal[3],bookVal[4],bookVal[5]);
-   
-    myLibrary.push(book);
-}
-
-// Function for publishing myLibrary
-
+// Function for publishing myLibrary into HTML
 function publishMyLibrary() {
-
     document.querySelector(`.book-wrap`).replaceChildren("");
-
     myLibrary.forEach((entry) => {
-
-        for(i = 0; i < 6; i++){
+        for(i = 0; i < 6; i++) {
             bookTemplate.children[i].innerText = Object.values(entry)[i];
         };
-
         let bookClone = bookTemplate.cloneNode(true);
         bookClone.classList.remove('hidden');
-
         document.querySelector('.book-wrap').append(bookClone);
-
     });
 }
 
 // Function for storing all published books into myLibrary
-
 function updateMyLibrary() {
-    let pubList = document.querySelectorAll(`.book-wrap .book`);
     myLibrary = [];
-
+    let pubList = document.querySelectorAll(`.book-wrap .book`);
     pubList.forEach((entry) => {
-        let title = entry.children[0].innerText;
-        let author = entry.children[1].innerText;
-        let year = entry.children[2].innerText;
-        let genre = entry.children[3].innerText;
-        let pages = entry.children[4].innerText;
-        let score = entry.children[5].innerText;
-
-        let book = new Book(title,author,year,genre,pages,score);
+        let book = new Book(
+            entry.children[0].innerText,
+            entry.children[1].innerText,
+            entry.children[2].innerText,
+            entry.children[3].innerText,
+            entry.children[4].innerText,
+            entry.children[5].innerText
+            );
         myLibrary.push(book);
     });
 }
 
-// Add button
-
+// 'Add' button
 document.querySelector('.add-button').addEventListener('click', ()=> {
     document.querySelector('.menu').classList.toggle('hidden');
     form.reset();
 });
 
-// Sort button
-
+// 'Sort' button
 document.querySelector('.sort-button').addEventListener('click', ()=> {
-    
     updateMyLibrary();
-
     myLibrary.sort((a,b) => a.title > b.title ? 1 : -1);
-
     publishMyLibrary();
 });
 
-// Create Entry button
-
+// 'Create Entry' button
 document.querySelector('.form-button').addEventListener('click', (event)=> {
-
     event.preventDefault();
-
     if(form.elements[0].value != ""){
-
-        addBookToLibrary();
+        let book = new Book(
+            form.elements[0].value,
+            form.elements[1].value,
+            form.elements[2].value,
+            form.elements[3].value,
+            form.elements[4].value,
+            form.elements[5].value
+        );
+        myLibrary.push(book);
         publishMyLibrary();
         form.reset();
-
     } else {
-        alert('Please add a title to your new entry!');
+        document.querySelector(`.title-dialog`).showModal();
     }
 });
 
-// Review Button
+// 'Review' Button
+document.querySelector(".book-wrap").addEventListener('click', (event)=> {
+    if(event.target.classList.contains('review-button')) {
+        lastReview = event.target.parentElement;
+        document.querySelector(`.review-dialog`).showModal();
+        document.querySelector(`#review`).placeholder = 
+        `${lastReview.children[5].innerText}`;
+    }
+});
 
+// 'Review Dialog' Button
+document.querySelector(`.dialog-button`).addEventListener(`click`, ()=> {
+    lastReview.children[5].innerText = document.querySelector(`#review`).value;
+    updateMyLibrary();
+    publishMyLibrary();
+});
 
-
-// Delete button
-
-document.querySelector(".book-wrap").addEventListener('click', (click)=> {
-    if(click.target.classList.contains('delete-button')) {
-        click.target.parentElement.remove();
+// 'Delete' button
+document.querySelector(".book-wrap").addEventListener('click', (event)=> {
+    if(event.target.classList.contains('delete-button')) {
+        event.target.parentElement.remove();
     }
     updateMyLibrary();
-
 });
+
